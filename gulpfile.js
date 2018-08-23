18dfaf627d108d438da7ebf29cf584f039e6bfc8
J.Paradiso <joseph.r.paradiso@gmail.com>
2018-08-23 14:52:10 -0400

    Added Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require("gulp"),
   gutil = require("gulp-util"),
   coffee = require("gulp-coffee"),
-  concat = require("gulp-concat"),
+  compass = require("gulp-compass"),
   browserify = require("gulp-browserify"),
-  compass = require("gulp-compass");
+  connect = require("gulp-connect"),
+  concat = require("gulp-concat"),
+  http = require("http");
 
 var coffeeSources = ["components/coffee/tagline.coffee"];
 var jsSources = [
@@ -13,6 +15,8 @@ var jsSources = [
   "components/scripts/template.js"
 ];
 var sassSources = ["components/sass/style.scss"];
+var htmlSources = ["builds/development/*.html"];
+var jsonSources = ["builds/development/js/*.json"];
 
 gulp.task("coffee", function() {
   gulp
@@ -26,7 +30,8 @@ gulp.task("js", function() {
     .src(jsSources)
     .pipe(concat("scripts.js"))
     .pipe(browserify())
-    .pipe(gulp.dest("builds/development/js"));
+    .pipe(gulp.dest("builds/development/js"))
+    .pipe(connect.reload());
 });
 
 gulp.task("compass", function() {
@@ -39,13 +44,46 @@ gulp.task("compass", function() {
         style: "expanded"
       }).on("error", gutil.log)
     )
-    .pipe(gulp.dest("builds/development/css"));
+    .pipe(gulp.dest("builds/development/css"))
+    .pipe(connect.reload());
 });
-
-gulp.task("default", ["coffee", "js", "compass", "watch"]);
 
 gulp.task("watch", function() {
   gulp.watch(coffeeSources, ["coffee"]);
   gulp.watch(jsSources, ["js"]);
   gulp.watch("components/sass/*.scss", ["compass"]);
+  gulp.watch(htmlSources, ["html"]);
+  gulp.watch(jsonSources, ["json"]);
+});
+
+gulp.task("connect", function() {
+  connect.server({
+    root: "builds/development/",
+    livereload: true
+  });
+});
+
+gulp.task("html", function() {
+  gulp.src(htmlSources).pipe(connect.reload());
+});
+
+gulp.task("json", function() {
+  gulp.src(jsonSources).pipe(connect.reload());
+});
+
+gulp.task("default", [
+  "html",
+  "json",
+  "coffee",
+  "js",
+  "compass",
+  "watch",
+  "connect"
+]);
+
+gulp.task("webserver-stop", function(cb) {
+  http
+    .request("http://localhost:8080/_kill_")
+    .on("close", cb)
+    .end();
 });
